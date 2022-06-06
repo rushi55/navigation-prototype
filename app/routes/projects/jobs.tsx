0e98b7f8b79005a7769affa643279cfb73d85371
Rushi Arumalla <rushi@mosaic.us>
2022-06-06 16:05:00 +0100

    missing content

---
+++ app/routes/projects/jobs.tsx
@@ -0,0 +1,8 @@
+export const Jobs = () => {
+    return (
+      <div>Jobs Content</div>
+    )
+  }
+  
+  export default Jobs
+  
---
+++ app/routes/projects/payments.tsx
@@ -0,0 +1,7 @@
+export const Payments = () => {
+  return (
+    <div>Payments Content</div>
+  )
+}
+
+export default Payments
